a31e7b5623b677f5ddaa935eeb51f983b02580b4
Peter Qin <Peter_Qin@live.com>
2016-06-05 23:44:12 +0800

    HelloSpring - add log

--- HelloSpring/src/hello/spring/JobController.java
+++ HelloSpring/src/hello/spring/JobController.java
@@ -1,14 +1,25 @@
 package hello.spring;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/Job")
 public class JobController {
- 
-    @RequestMapping(params="action=getJob")
-    public String getJob(){        
+	private static Log FLog = LogFactory.getLog(JobController.class);
+	
+	@RequestMapping
+    public String DisplayJob(){   
+        return "DisplayJob";
+    }
+	
+    @RequestMapping(params="action=GetJob")
+    public String GetJob(@RequestParam String aJobID, Model aModel){   
+    	FLog.info("Job ID: " + aJobID); 
         return "getJob";
     }
 }
